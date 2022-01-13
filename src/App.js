284058c3f5a90ddb4f9cc64c2bd0ab1fc6617d01
Seungho <poream3387@gmail.com>
2022-01-13 22:17:09 +0900

    Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/pages/Hero/Hero";
 import { GlobalStyle } from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 function App() {
     return (
         <>
-            <GlobalStyle />
-            <Navbar />
-            <Hero />
-            <div>sfsdf</div>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <Navbar />
+                <Hero />
+            </ThemeProvider>
         </>
     );
 }
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+    colors: {
+        blueberry: "#5680E9",
+        lightBlue: "#84CEEB",
+        skyBlue: "#5AB9EA",
+        gray: "#C1C8E4",
+        violet: "#8860D0"
+    }
+}
+
+export default theme;
