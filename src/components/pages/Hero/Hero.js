9f1c664d8dafff9a9bb12d46bd6a63c1ce7c7a4b
Seungho <poream3387@gmail.com>
2022-01-14 13:32:40 +0900

    Fix purple dashed spare space issue

--- src/components/pages/Hero/Hero.js
+++ src/components/pages/Hero/Hero.js
@@ -1,32 +1,118 @@
 import React from "react";
+import { IoIosFlash } from "react-icons/io";
 import styled from "styled-components";
+import studyImg from "../../../asset/casual-life-3d-reading.png";
+
+const heroColor = ({ theme }) => theme.colors.blueberry;
+const navyColor = ({ theme }) => theme.colors.navy;
+const whiteColor = ({ theme }) => theme.colors.white;
 
 const data = {
-    Slogan: "Learn faster with Memozi",
-    CTA: "Sign Up Now!",
+    heroMessage: "Memorize and Learn like thunder",
+    CTAMessage:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    CTAButtonText: "Get Started",
 };
 
+const breakPoint = "955px";
+
 const HeroSection = styled.div`
-    background: red;
-    height: 60vh;
+    padding: 160px 0;
+    background: ${heroColor};
+    height: 45vh;
 `;
 
 const HeroContainer = styled.div`
-    overflow: hidden;
+    z-index: 1;
+    width: 100%;
+    max-width: 1500px;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 50px;
+    padding-left: 50px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: stretch;
+    align-items: center;
+    flex-wrap: wrap;
 `;
 
-const HeroTextWrapper = styled.div``;
+const Row = styled.div`
+    z-index: 1;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    align-content: stretch;
+`;
 
-const HeroImageWrapper = styled.div``;
+const Col = styled.div`
+    margin-bottom: 15px;
+    padding-right: 20px;
+    padding-left: 20px;
+    flex: 1;
+    max-width: 50%;
+    flex-basis: 50%;
+`;
+
+const HeroTextWrapper = styled.div`
+    padding-top: 0;
+    padding-bottom: 60px;
+    max-width: 600px;
+`;
+
+const HeroImageWrapper = styled.div`
+`;
+
+const HeroMessage = styled.h1`
+    color: ${whiteColor};
+    font-size: 60px;
+    margin-bottom: 30px;
+`;
+
+const HeroImage = styled.img`
+    margin-top: 0;
+    margin-right: 0;
+    margin-left: 10px;
+    padding-right: 0;
+    border: 0;
+    max-width: 100%;
+    vertical-align: middle;
+    display: inline-block;
+`;
+
+const CTAMessage = styled.h4`
+    color: ${whiteColor};
+    margin-bottom: 50px;
+`;
 
 const CTAWrapper = styled.div``;
+
+const CTAButton = styled.button`
+    background: transparent;
+    color: ${whiteColor};
+    font-size: 14px;
+    border-color: ${whiteColor};
+    border-style: solid;
+    border-width: 2px;
+    border-radius: 22px;
+    padding: 10px 40px;
+    text-transform: uppercase;
+    transition: all 0.2s linear;
+
+    &:hover {
+        background: ${navyColor};
+        cursor: pointer;
+        border-color: ${navyColor};
+        transition: all 0.2s linear;
+    }
+`;
 
 const Wave = styled.svg`
     stroke-opacity: 0;
 `;
 
 const WavePath = styled.path`
-    fill: red;
+    fill: ${heroColor};
 `;
 
 function Hero() {
@@ -34,18 +120,29 @@ function Hero() {
         <>
             <HeroSection>
                 <HeroContainer>
-                    <HeroTextWrapper>
-                        <h4>{data.Slogan}</h4>
-                    </HeroTextWrapper>
-                    <HeroImageWrapper></HeroImageWrapper>
-                    <CTAWrapper>
-                        <button>Play store</button>
-                        <button>App Store</button>
-                    </CTAWrapper>
+                    <Row>
+                        <Col>
+                            <HeroTextWrapper>
+                                <HeroMessage>
+                                    {data.heroMessage}
+                                    <IoIosFlash color="yellow" />
+                                </HeroMessage>
+                                <CTAWrapper>
+                                    <CTAMessage>{data.CTAMessage}</CTAMessage>
+                                    <CTAButton>{data.CTAButtonText}</CTAButton>
+                                </CTAWrapper>
+                            </HeroTextWrapper>
+                        </Col>
+                        <Col>
+                            <HeroImageWrapper>
+                                <HeroImage src={studyImg} alt="Study Image" />
+                            </HeroImageWrapper>
+                        </Col>
+                    </Row>
                 </HeroContainer>
             </HeroSection>
             <Wave viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-                <WavePath d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z"></WavePath>
+                <WavePath d="M0,50 C100,150 250,0 500,70 L500,00 L0,0 Z"></WavePath>
             </Wave>
         </>
     );
--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,9 @@ const theme = {
         lightBlue: "#84CEEB",
         skyBlue: "#5AB9EA",
         gray: "#C1C8E4",
-        violet: "#8860D0"
+        violet: "#8860D0",
+        white: "#fff",
+        navy: "#1C2237"
     }
 }
 
